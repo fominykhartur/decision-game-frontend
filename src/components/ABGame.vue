52637322a392b3aaf66ed9872f175de8e2f8cfc5
fominykhartur <fominykhartur+GitHub@gmail.com>
2022-09-07 00:24:24 +0300

    Переключение комнат

--- src/components/ABGame.vue
+++ src/components/ABGame.vue
@@ -9,6 +9,7 @@ export default {
     return {
       body : 0,
       chatMessages : [],
+      rooms : [],
       clientsList : [],
       currentRoom: "",
       LobbyStatus: true,
@@ -25,6 +26,11 @@ export default {
       this.socket.emit(`createRoom`, this.currentRoom, roomName)
       this.currentRoom = roomName
       // this.chatMessages = []
+    },
+    joinRoom: function(room) {
+      console.log('Переход в комнату ',room)
+      this.socket.emit('changeRoom', this.currentRoom, room)
+      this.currentRoom = room
     },
     sendMessage : function(inputMessage) {
       console.log(`Отправлено сообщение ${inputMessage}`)
@@ -44,6 +50,11 @@ export default {
       console.log('Connected clients: ', data)
       this.clientsList = data
     });
+
+    this.socket.on('getRooms', (rooms) => {
+      console.log('rooms', rooms)
+      this.rooms = rooms
+    })
 
     this.socket.on('getChatMessage', (chatMessage) => {
       this.chatMessages.push(chatMessage)
@@ -93,7 +104,7 @@ export default {
   <div>
     <input ref="inputChatMessage" v-model.trim="inputMessage" @keyup.enter="sendMessage(inputMessage)">
   </div>
-  <div v-if="body >= 2">
-    <p><button v-on:click="turnOffLobby(!LobbyStatus)">Ally</button></p>
+  <div v-for="room in rooms" :key="room">
+    <button v-on:click="joinRoom(room)">{{room}}</button>
   </div>
 </template>
